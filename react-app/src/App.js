289f41d9b6fae31b717916283c830b8645ad419b
WillCZhang <will.chi.zhang@gmail.com>
2019-05-29 17:20:15 -0700

    added react & meteor app

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './components/Button';
+
+const App = () => {   //this is how you make a functional component
+	return (
+		<div><Button/></div>
+	);
+}
+
+
+export default App;
