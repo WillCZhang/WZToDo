dc48a7fbcc59cb8c709d8d6831349c4902d63795
WillCZhang <will.chi.zhang@gmail.com>
2019-05-29 17:04:36 -0700

    adding button page

--- react-app/src/components/Button.js
+++ react-app/src/components/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Button extends React.Component {
 	render() {
@@ -6,4 +7,4 @@ class Button extends React.Component {
 	}
 }
 
-export default Button;
+export default connect()(Button); 
