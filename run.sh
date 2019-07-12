c8d3dcff53aa7e51b124958fcb3f3ad30090f417
WillCZhang <will.chi.zhang@gmail.com>
2019-07-11 22:12:26 -0700

    modifying run script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # This script starts both react frontend & express.js backend server in two subshells
 
+# Install dependencies...
+npm install && cd ./client && npm install && cd ..
+
+# Starting both processes in subshells
 ( npm start ) & ( cd ./client && npm start )
