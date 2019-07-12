fee8bb87259f84de013b267bcf212c6096fb45bc
WillCZhang <will.chi.zhang@gmail.com>
2019-07-11 23:19:27 -0700

    adding production build

---
+++ client/build/precache-manifest.c6652f73ad2ea49b0a4863612f4cee6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81445cd49f815e2c6b859e6f68f219f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6b8168492ef46d41029",
+    "url": "/static/css/main.2d4becd8.chunk.css"
+  },
+  {
+    "revision": "2672504a52bbeb9b04a8",
+    "url": "/static/js/2.fb28baff.chunk.js"
+  },
+  {
+    "revision": "e6b8168492ef46d41029",
+    "url": "/static/js/main.d5b909c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
